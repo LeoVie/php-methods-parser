ba9725b8b763f6df75b1ad5027766bcb060046e9
Leo Viezens <leo.viezens@web.de>
2021-10-22 13:16:40 +0200

    Fix namespace

--- src/DependencyInjection/PhpMethodsParserExtension.php
+++ src/DependencyInjection/PhpMethodsParserExtension.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace LeoVie\PhpTokenNormalize\DependencyInjection;
+namespace LeoVie\PhpMethodsParser\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
